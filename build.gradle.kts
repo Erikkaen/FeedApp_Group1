e499d6fc406dc5e9d203105bb53093cf0b00dbf1
guangqingz <zgq3@hotmail.com>
2025-11-04 11:54:05 +0100

    Added Front-end and tested H2

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	runtimeOnly("com.h2database:h2")
+	implementation("com.h2database:h2:2.3.232")
+	implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
+	implementation("org.hibernate.orm:hibernate-core:7.1.1.Final")
 }
 
 tasks.withType<Test> {
