242bc835d63b033778a093e3af72e965fdbf9cd7
Adele Iren Westrum Kjølstad <aikjolst@stud.ntnu.no>
2025-11-12 20:30:19 +0100

    Added some tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // H2 database
 	runtimeOnly("com.h2database:h2")
 	implementation("com.h2database:h2:2.3.232")
+
+    // Postgres database
+//    implementation("org.postgresql:postgresql:42.7.7")
+//    runtimeOnly("org.postgresql:postgresql")
+
 	implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
 	implementation("org.hibernate.orm:hibernate-core:7.1.1.Final")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.5")
@@ -32,4 +38,7 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
+    }
 }
