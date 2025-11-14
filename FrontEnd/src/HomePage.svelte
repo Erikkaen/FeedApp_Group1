fddb8192af7a46aeb2c15c9be34d7a3c05b5aa1e
FredrikM2000 <70491657+FredrikM2000@users.noreply.github.com>
2025-11-14 13:20:16 +0100

    Frontend update

--- FrontEnd/src/HomePage.svelte
+++ FrontEnd/src/HomePage.svelte
@@ -18,21 +18,23 @@
 </script>
 
 <main>
-    <h1>FeedApp</h1>
-    <h2>Your very own poll app!</h2>
-
-    <!-- Go to registration -->
-    <div class="homeComponent">
-        <h3>Don't have an account?</h3>
-        <button class="homeButton" on:click={register}>Register account</button>
+    <div class="homepage">
+        <h1>FeedApp</h1>
+        <h2>Your very own poll app!</h2>
+        
+        <!-- Go to registration -->
+        <div class="homeComponent">
+            <h3>Don't have an account?</h3>
+            <button class="homeButton" on:click={register}>Register account</button>
+        </div>
+    
+        <!-- Login -->
+        <div class="homeComponent">
+        <h3>Already have an account?</h3>
+        <button class="homeButton" on:click={login}>Login</button>
+        </div>
+    
+        <!-- continue as guest -->
+        <button class="homeButton" on:click={guest}>Continue as Guest</button>
     </div>
-
-    <!-- Login -->
-    <div class="homeComponent">
-    <h3>Already have an account?</h3>
-    <button class="homeButton" on:click={login}>Login</button>
-    </div>
-   
-    <!-- continue as guest -->
-    <button class="homeButton" on:click={guest}>Continue as Guest</button>
 </main>
